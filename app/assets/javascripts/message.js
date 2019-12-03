3fae09218dabcaccf982703a5730ed1e83d453f0
yunakajimayu <y_nakajima1002@icloud.com>
2019-12-03 21:53:52 +0900

    fixed

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -37,7 +37,6 @@ $(function(){
     .done(function(data){
       var html = buildHTML(data);
       $('.messages').append(html);
-      $('#message_content').val('');
       $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
       $("#new_message")[0].reset();
     })
