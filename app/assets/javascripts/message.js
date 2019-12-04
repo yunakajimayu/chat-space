95c73138359c3312c06078290b9216b840050f42
yunakajimayu <y_nakajima1002@icloud.com>
2019-12-04 22:58:32 +0900

    fixed

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,50 @@
+$(function(){
+  function buildHTML(message) {
+    var content = message.content ? `${ message.content }` : "";
+    var img = message.image ? `<img src= ${ message.image }>` : "";
+    var html = `<div class="message" data-id="${message.id}">
+                  <div class="upper-message">
+                    <div class="upper-message__current-user-name">
+                      ${message.user_name}
+                    </div>
+                    <div class="upper-message__date">
+                      ${message.date}
+                    </div>
+                  </div>
+                  <p class="lower-message">
+                    <div>
+                      ${content}
+                    </div>
+                    <div>
+                      ${img}
+                    </div>
+                  </p>
+                </div>`
+  return html;
+  }
+  $('#new_message').on('submit', function(e){
+    e.preventDefault();
+    var formData = new FormData(this);
+    var url = $(this).attr('action')
+    $.ajax({  
+      url: url,
+      type: 'POST',
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.messages').append(html);
+      $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
+      $("#new_message")[0].reset();
+    })
+    .fail(function(data){
+      alert('エラーが発生したためメッセージは送信できませんでした。');
+    })
+    .always(function(data){
+    $('.form__submit').prop('disabled', false);
+    })
+  })
+});
