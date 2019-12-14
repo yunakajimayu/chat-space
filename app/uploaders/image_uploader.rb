315d9305bcb52803cdacfebb209b5f0d45fec53b
yunakajimayu <y_nakajima1002@icloud.com>
2019-12-14 22:17:06 +0900

    fixed

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,16 +1,19 @@
+# encoding: utf-8
+
 class ImageUploader < CarrierWave::Uploader::Base
+
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   include CarrierWave::MiniMagick
   process resize_to_fit: [800, 800]
+
   # Choose what kind of storage to use for this uploader:
-  storage :file
-  # storage :fog
-  
+  storage :fog
+
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    "uploads/# {model.class.to_s.underscore}/# {mounted_as}/# {model.id}"
   end
 
   # Provide a default URL as a default if there hasn’t been a file uploaded:
